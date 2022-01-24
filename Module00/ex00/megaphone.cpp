a4b19e8e257c083fffc6e075e82b1903701aeac1
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-01-24 19:37:55 +0100

    Added includes for the other methods I used from the std namespace. Don't know why they weren't required by the compiler, perhaps other OS's do require them to be explicitly included? So have left them as a comment for now. Also used the specifically c++ variant of the c function toupper, which is sensitive to the user's locale.

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -6,11 +6,13 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/24 15:48:02 by limartin      #+#    #+#                 */
-/*   Updated: 2022/01/24 19:19:15 by limartin      ########   odam.nl         */
+/*   Updated: 2022/01/24 19:33:17 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+// #include <string> // std::string
+// #include <locale> // std::locale, std::toupper
 
 int main(int argc, char **argv)
 {
@@ -22,8 +24,9 @@ int main(int argc, char **argv)
 		for (int a = 1; a < argc ; a++)
 		{
 			std::string strToUpper = argv[a];
+			std::locale loc;
 			for (std::string::iterator i = strToUpper.begin(); i < strToUpper.end(); i++)
-				*i = std::toupper(*i);
+				*i = std::toupper(*i, loc);
 			std::cout << strToUpper;
 		}
 	}
