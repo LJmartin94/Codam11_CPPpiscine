0c01d7106293f22e346d0377d88ddc4c939e14ee
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-01-24 19:29:55 +0100

    Tried to solve this exercise in the most cpp way possible, so using string objects and their attributes instead of C's regular null terminated character arrays and an index. Also used the built-in toupper method rather than manually checking whether a char is a-z and then increasing its ASCII value.

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -6,14 +6,27 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/24 15:48:02 by limartin      #+#    #+#                 */
-/*   Updated: 2022/01/24 17:17:59 by limartin      ########   odam.nl         */
+/*   Updated: 2022/01/24 19:19:15 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	(void)argv;
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int a = 1; a < argc ; a++)
+		{
+			std::string strToUpper = argv[a];
+			for (std::string::iterator i = strToUpper.begin(); i < strToUpper.end(); i++)
+				*i = std::toupper(*i);
+			std::cout << strToUpper;
+		}
+	}
+	std::cout << std::endl; 
 	return (0);
 }
