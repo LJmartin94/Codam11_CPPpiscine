4dd1a02416168429e38afd036d55e69172b1ae1b
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-01-25 16:38:21 +0100

    Got rid of the switch statement (couldn't make the cases non-dynamic, so had no way to compare the buff to multiple strings). Made a jump table. I imagine a lot of this code will move to the Phonebook Class.

--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -6,21 +6,49 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/24 21:03:00 by limartin      #+#    #+#                 */
-/*   Updated: 2022/01/25 14:06:24 by limartin      ########   odam.nl         */
+/*   Updated: 2022/01/25 16:31:59 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+// #include <stdlib.h> //Exit, EXIT_SUCCESS
+
+int ft_add()
+{
+	std::cout << "Contact added." << std::endl;
+	return (0);
+}
+
+int ft_search()
+{
+	std::cout << "Looking up contact." << std::endl;
+	return (0);
+}
+
+int ft_exit()
+{
+	std::cout << "Exiting program" << std::endl;
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int ft_error()
+{
+	std::cout << "BAD INPUT, TRY: ADD || SEARCH || EXIT" << std::endl;
+	return (0);
+}
 
 int main()
 {
 	enum CommandType { ADD, SEARCH, EXIT, ERROR};
+	int (*commands[])() = {ft_add, ft_search, ft_exit, ft_error};
 	CommandType inputMatch;
 
 	while(true)
 	{
 		std::string buf;
 		std::cin >> buf;
+
 		inputMatch = ERROR;
 		if (buf.compare("ADD") == 0)
 			inputMatch = ADD;
@@ -28,22 +56,8 @@ int main()
 			inputMatch = SEARCH;
 		else if (buf.compare("EXIT") == 0)
 			inputMatch = EXIT;
-		switch(inputMatch)
-		{
-			case ADD:
-				std::cout << "Contact added." << std::endl;
-				break;
-			case SEARCH:
-				std::cout << "Looking up contact." << std::endl;
-				break;
-			case EXIT:
-				std::cout << "Exiting program" << std::endl;
-				return (0);
-			case ERROR:
-				std::cout << "ERROR" << std::endl;
-				break;
-		}
-		std::cout << std::endl;
+		
+		commands[inputMatch]();
 	}
 	return (0);
 }
