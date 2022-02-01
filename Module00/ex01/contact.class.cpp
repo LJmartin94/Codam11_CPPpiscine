614573d71e2cb5d5a3eb009b8ff47638b65fe65e
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-02-01 19:49:13 +0100

    Wrote getters and setters for all contact's private attributes.

--- Module00/ex01/contact.class.cpp
+++ Module00/ex01/contact.class.cpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/01 17:40:37 by limartin      #+#    #+#                 */
-/*   Updated: 2022/02/01 17:44:36 by limartin      ########   odam.nl         */
+/*   Updated: 2022/02/01 19:43:06 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,61 @@ Contact::~Contact(void)
 	std::cout << "Contact destroyed." << std::endl;
 	return;
 }
+
+int				Contact::set_firstName(std::string input)
+{
+	this->_firstName = input;
+	return(0);
+}
+
+std::string		Contact::get_firstName()
+{
+	return(this->_firstName);
+}
+
+int				Contact::set_lastName(std::string input)
+{
+	this->_lastName = input;
+	return(0);
+}
+
+std::string		Contact::get_lastName()
+{
+	return(this->_lastName);
+}
+
+int				Contact::set_nickname(std::string input)
+{
+	this->_nickname = input;
+	return(0);
+}
+
+std::string		Contact::get_nickname()
+{
+	return(this->_nickname);
+}
+
+int				Contact::set_phoneNumber(std::string input)
+{
+	this->_phoneNumber = input;
+	return(0);
+}
+
+std::string		Contact::get_phoneNumber()
+{
+	return(this->_phoneNumber);
+}
+
+int				Contact::set_secret(std::string input)
+{
+	this->_darkestSecret = input;
+	return(0);
+}
+
+std::string		Contact::get_secret()
+{
+	return(this->_darkestSecret);
+}
+
 
 // PRIVATE
--- Module00/ex01/contact.class.hpp
+++ Module00/ex01/contact.class.hpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/24 21:15:55 by limartin      #+#    #+#                 */
-/*   Updated: 2022/02/01 17:36:14 by limartin      ########   odam.nl         */
+/*   Updated: 2022/02/01 19:43:03 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,19 @@
 class Contact
 {
 	public:
-	Contact(void);
-	~Contact(void);
+	Contact();
+	~Contact();
+
+	int set_firstName(std::string input);
+	std::string get_firstName();
+	int set_lastName(std::string input);
+	std::string get_lastName();
+	int set_nickname(std::string input);
+	std::string get_nickname();
+	int set_phoneNumber(std::string input);
+	std::string get_phoneNumber();
+	int set_secret(std::string input);
+	std::string get_secret();
 
 	private:
 	std::string _firstName;
