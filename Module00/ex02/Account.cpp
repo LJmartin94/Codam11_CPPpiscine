1d71868b4196445e69eac9bf69f68f39eacf8cce
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-02-07 16:11:11 +0100

    Added empty bodies to all the prototypes from the .hpp file in the .cpp file.

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,7 +6,84 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/07 14:30:40 by limartin      #+#    #+#                 */
-/*   Updated: 2022/02/07 14:30:41 by limartin      ########   odam.nl         */
+/*   Updated: 2022/02/07 16:10:18 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+
+// PUBLIC
+
+Account::Account( int initial_deposit )
+{
+	return ;
+}
+
+Account::~Account( void )
+{
+	return ;
+}
+
+int	Account::getNbAccounts( void )
+{
+	return ;
+}
+
+int	Account::getTotalAmount( void )
+{
+	return ;
+}
+
+int	Account::getNbDeposits( void )
+{
+	return ;
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return ;
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	return ;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	return ;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	return ;
+}
+
+int		Account::checkAmount( void ) const
+{
+	return ;
+}
+
+void	Account::displayStatus( void ) const
+{
+	return ;
+}
+
+
+
+// PRIVATE
+
+// 	static int	_nbAccounts;
+// 	static int	_totalAmount;
+// 	static int	_totalNbDeposits;
+// 	static int	_totalNbWithdrawals;
+
+// 	int				_accountIndex;
+// 	int				_amount;
+// 	int				_nbDeposits;
+// 	int				_nbWithdrawals;
+
+void	Account::_displayTimestamp( void )
+{
+	return ;
+}
