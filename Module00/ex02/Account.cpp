b30f7f6cc55b94a5d0b95a132107e367df9e1ed6
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-02-09 21:53:30 +0100

    Realised it wasn't working because the 'static' variables are non-member variables, so are created without being instantiated in any one instance of the class. Because we can't change the .hpp file we can just declare them at the top of the cpp file, and now the previous syntax I had written for the accessors works. Quickly wrote the constructor fir the instances of the Account class (though could re-write it to use an initialisation list too, which might be nicer).

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,20 +6,31 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/07 14:30:40 by limartin      #+#    #+#                 */
-/*   Updated: 2022/02/08 17:23:24 by limartin      ########   odam.nl         */
+/*   Updated: 2022/02/09 21:14:20 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 
-// PUBLIC
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
+
+// PUBLIC
 Account::Account( int initial_deposit )
-{
-	(void) initial_deposit;
+{	
+	this-> _accountIndex = _nbAccounts;
+	Account::_nbAccounts++;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
 	
-	this->_displayTimestamp();
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
 	std::cout << "created" << std::endl;
 	return ;
 }
@@ -33,26 +44,22 @@ Account::~Account( void )
 
 int	Account::getNbAccounts( void )
 {
-	// return (Account::_nbAccounts);
-	return (0);
+	return (Account::_nbAccounts);
 }
 
 int	Account::getTotalAmount( void )
 {
-	// return (Account::_totalAmount);
-	return (0);
+	return (Account::_totalAmount);
 }
 
 int	Account::getNbDeposits( void )
 {
-	// return (Account::_totalNbDeposits);
-	return (0);
+	return (Account::_totalNbDeposits);
 }
 
 int	Account::getNbWithdrawals( void )
 {
-	// return (Account::_totalNbWithdrawals);
-	return (0);
+	return (Account::_totalNbWithdrawals);
 }
 
 void	Account::displayAccountsInfos( void )
@@ -90,19 +97,7 @@ void	Account::displayStatus( void ) const
 }
 
 
-
 // PRIVATE
-
-// 	static int	_nbAccounts;
-// 	static int	_totalAmount;
-// 	static int	_totalNbDeposits;
-// 	static int	_totalNbWithdrawals;
-
-// 	int				_accountIndex;
-// 	int				_amount;
-// 	int				_nbDeposits;
-// 	int				_nbWithdrawals;
-
 void	Account::_displayTimestamp( void )
 {
 	std::cout << "[TIME_STAMP] ";
