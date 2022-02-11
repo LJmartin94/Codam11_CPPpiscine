9d8c82d14fdbb5f6c870dde29b07bfb77ebf94eb
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-02-11 19:19:05 +0100

    Put all the endlines on a new line wherever they occur within the body of a method. Need to fix the time stamp method next, and then I might be done? But I don't know what I was supposed to use the checkAmount method for, as it is never called by tests.cpp

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/07 14:30:40 by limartin      #+#    #+#                 */
-/*   Updated: 2022/02/11 19:08:36 by limartin      ########   odam.nl         */
+/*   Updated: 2022/02/11 19:16:33 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ Account::Account( int initial_deposit )
 	
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
-	std::cout << "created" << std::endl;
+	std::cout << "created";
+	std::cout << std::endl;
 	return ;
 }
 
@@ -38,7 +39,8 @@ Account::~Account( void )
 	this->_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
-	std::cout << "closed" << std::endl;
+	std::cout << "closed";
+	std::cout << std::endl;
 	return ;
 }
 
@@ -68,7 +70,8 @@ void	Account::displayAccountsInfos( void )
 	std::cout << "accounts:" << getNbAccounts() << ";";
 	std::cout << "ammount:" << getTotalAmount() << ";";
 	std::cout << "deposits:" << getNbDeposits() << ";";
-	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
+	std::cout << "withdrawals:" << getNbWithdrawals();
+	std::cout << std::endl;
 	return ;
 }
 
@@ -112,7 +115,8 @@ bool	Account::makeWithdrawal( int withdrawal )
 
 int		Account::checkAmount( void ) const
 {
-	std::cout << "check" << std::endl;
+	std::cout << "check";
+	std::cout << std::endl;
 	return (0);
 }
 
