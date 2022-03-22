2677e6087af31fa31f1ff8ac138bfd13472a2b47
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-03-22 21:13:29 +0100

    Added zombieHorde.cpp to Makefile. Called zombieHorde function in main. Trying to figure out the syntax for zombieHorde, don't know how to make an array of objects that don't have a default constructor without using vectors.

--- Module01/ex01/main.cpp
+++ Module01/ex01/main.cpp
@@ -6,13 +6,17 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/22 16:15:24 by limartin      #+#    #+#                 */
-/*   Updated: 2022/03/22 19:55:32 by limartin      ########   odam.nl         */
+/*   Updated: 2022/03/22 20:19:16 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+Zombie*	zombieHorde( int N, std::string name );
+
 int		main(void)
 {
+	Zombie *horde = zombieHorde(3, "Bob");
+	horde->announce();
 	return (0);
 }
---
+++ Module01/ex01/zombieHorde.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   zombieHorde.cpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: limartin <limartin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/03/22 19:59:36 by limartin      #+#    #+#                 */
+/*   Updated: 2022/03/22 21:10:12 by limartin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+#include <vector>
+
+Zombie*	zombieHorde( int N, std::string name )
+{
+	//Can't do this for a number of reasons. 
+	//One problem we won't be able to solve is that variable length arrays are C99
+	//		Zombie horde[N];
+
+	//Can't do this because no default constructor exists for class Zombie
+	//		Zombie* horde = NULL;
+	// 		horde = new Zombie[N];
+
+	//This works as a pointer to a single Zombie, but we need N zombies allocated at once.
+	//		Zombie* horde = NULL;
+	//		horde = new Zombie(name);
+
+	//This gives two error messages, one relating to 'new' and one relating to '[N]'
+	Zombie* horde = NULL;
+	horde = new (Zombie(name)[N]);
+
+	//Look at this tomorrow:
+	// https://en.cppreference.com/w/cpp/language/new
+
+	// Zombie (horde[N])(name);
+	// Zombie (*horde)(name) = (new Zombie[N]);
+	// *horde = (new Zombie(name))[N];
+	// *horde = 
+
+	// std::vector<Zombie> h(N, Zombie(name));
+	// Zombie* horde = &(h[0]);
+	return (horde);
+}
