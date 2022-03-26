661edad3e06d5943f1a20813317871776d93f7bf
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-03-26 15:30:09 +0100

    Finished the exercise. As far as I understand references really do just function as aliases to existing variables, but I guess they can be useful for passing a variable outside the scope in which it was declared.

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/25 15:41:58 by limartin      #+#    #+#                 */
-/*   Updated: 2022/03/25 19:07:07 by limartin      ########   odam.nl         */
+/*   Updated: 2022/03/26 15:26:28 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,14 @@
 int	main(void)
 {
 	std::string stringVAR = "HI THIS IS BRAIN";
-	// std::string* stringPTR = &stringVAR;
-	// std::string& stringREF = stringVAR;
-	int var = 42;
-	int* ptr = &var;
-	int& ref = var;
+	std::string* stringPTR = &stringVAR;
+	std::string& stringREF = stringVAR;
 
-	std::cout << stringVAR << std::endl;
-	std::cout << "Address of var      " << &var << std::endl;
-	std::cout << "Address held by ptr " << ptr << std::endl;
-	std::cout << "Address held by ref " << &ref << std::endl;
-	var = 3;
-	std::cout << "Address of var      " << &var << std::endl;
-	std::cout << "Address held by ptr " << ptr << std::endl;
-	std::cout << "Address held by ref " << &ref << std::endl;
+	std::cout << "Address of stringVAR      " << &stringVAR << std::endl;
+	std::cout << "Address held by stringPTR " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF " << &stringREF << std::endl;
+	std::cout << "Value of stringVAR            " << stringVAR << std::endl;
+	std::cout << "Value pointed to by stringPTR " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF " << stringREF << std::endl;
 	return (0);
 }
