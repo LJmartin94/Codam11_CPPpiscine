8ca3a9da897b27c413bc27453cb369465928379c
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-01 17:13:00 +0200

    Added the empty scopes for all Harls methods.

--- Module01/ex05/Harl.cpp
+++ Module01/ex05/Harl.cpp
@@ -6,7 +6,48 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/01 16:46:33 by limartin      #+#    #+#                 */
-/*   Updated: 2022/04/01 16:46:34 by limartin      ########   odam.nl         */
+/*   Updated: 2022/04/01 17:10:50 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+// PUBLIC:
+
+Harl::Harl()
+{
+	return;
+}
+
+Harl::~Harl()
+{
+	return;
+}
+
+void	complain ( std::string level )
+{
+	(void)level;
+	return;
+}
+
+// PRIVATE:
+
+void	debug( void )
+{
+	return;
+}
+
+void	info( void )
+{
+	return;
+}
+
+void	warning( void )
+{
+	return;
+}
+
+void	error( void )
+{
+	return;
+}
