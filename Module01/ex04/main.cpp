eb7f906b16997fffb7eea0c2ff170d987223ca27
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-01 15:05:01 +0200

    Refactored existing error checks to be their own functions. Wrote error check for output file.

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,33 +6,59 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/31 14:37:36 by limartin      #+#    #+#                 */
-/*   Updated: 2022/04/01 13:36:57 by limartin      ########   odam.nl         */
+/*   Updated: 2022/04/01 15:02:42 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
 
-int main(int argc, char ** argv)
+int error_argcount(int argc, char **argv, std::string& filename, \
+std::string& s1, std::string& s2)
 {
 	if (argc != 4)
 	{
 		std::cout << "Please provide exactly three arguments" << std::endl;
 		return (1);
 	}
-	std::string filename = argv[1];
-	std::string s1 = argv[2];
-	std::string s2 = argv[3];
+	filename = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];
+	return (0);
+}
 
-	std::ifstream input(filename);
+int error_iofiles(std::ifstream& input, std::ofstream& output, std::string filename)
+{
+	input.open(filename, std::ifstream::in);
 	if (!input.good())
 	{
 		std::cout << "File with name \"" << filename;
 		std::cout << "\" does not exist or is fucky. #Awkward" << std::endl;
 		return (1);
 	}
-	std::ofstream output(filename.append(".replace"));
-	//should do output error checking too
+	output.open(filename.append(".replace"), std::ofstream::out);
+	if (!output.good())
+	{
+		std::cout << "There was a problem creating/reading from \"" << filename;
+		std::cout << "\"" << std::endl;
+		return (1);
+	}
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	std::string filename;
+	std::string s1;
+	std::string s2;
+	if (error_argcount(argc, argv, filename, s1, s2))
+		return (1);
+
+	std::ifstream input;
+	std::ofstream output;
+	if (error_iofiles(input, output, filename))
+		return (1);
+	
 	while (!input.eof())
 	{
 		std::string buf;
