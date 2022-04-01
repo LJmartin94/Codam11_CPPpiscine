ca6f14c5524a1a687119dbbcdbd8e73e4efc1b03
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-01 17:05:19 +0200

    Populated the hpp file with the basic class definition specified by the subject.

--- Module01/ex05/Harl.hpp
+++ Module01/ex05/Harl.hpp
@@ -6,7 +6,28 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/01 16:46:31 by limartin      #+#    #+#                 */
-/*   Updated: 2022/04/01 16:46:36 by limartin      ########   odam.nl         */
+/*   Updated: 2022/04/01 17:00:12 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <string>  //string datatype
+
+class Harl
+{
+	public:
+	Harl();
+	~Harl();
+
+	void	complain ( std::string level );
+	
+	private:
+	void	debug( void );
+	void	info( void );
+	void	warning( void );
+	void	error( void );
+};
+
+#endif
