e787d6f4ef553294fef8919b485f40b28123aab3
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-03 17:09:53 +0200

    They want us to use a switch case, and this implementation works but is quite ugly. I have an idea for how to improve it however.

--- Module01/ex06/main.cpp
+++ Module01/ex06/main.cpp
@@ -6,22 +6,69 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/01 16:46:28 by limartin      #+#    #+#                 */
-/*   Updated: 2022/04/03 16:03:46 by limartin      ########   odam.nl         */
+/*   Updated: 2022/04/03 17:04:51 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main(void)
+int	error_badInput()
 {
-	Harl karl;
-	karl.complain("ERR");
-	karl.complain("error");
-	karl.complain("lmao");
+	std::cout << "[ Probably complaining about";
+	std::cout << " insignificant problems ]" << std::endl;
+	return 1;
+}
+
+void complaintStrToEnum( const Harl& karl,\
+ const std::string& level, Harl::ComplaintType& type)
+{
+	for (int i = 0; i < Harl::SIZE; i++)
+	{
+		if (!(level.compare(karl.lookupTable[i])))
+		{
+			type = static_cast<Harl::ComplaintType>(i);
+			break;
+		}
+	}
+	return;
+}
+
+// void complaintWrapper( Harl::ComplaintType& type, const Harl& karl )
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		return(error_badInput());
 	
-	karl.complain("INFO");
-	karl.complain("DEBUG");
-	karl.complain("WARNING");
-	karl.complain("ERROR");
+	Harl karl;
+	std::string level = argv[1];
+	Harl::ComplaintType type = Harl::NA;
+	complaintStrToEnum(karl, level, type);
+	
+	switch(type)
+	{
+		case Harl::NA:
+			return(error_badInput());
+		case Harl::DEBUG:
+			karl.complain("DEBUG");
+			karl.complain("INFO");
+			karl.complain("WARNING");
+			karl.complain("ERROR");
+			break;
+		case Harl::INFO:
+			karl.complain("INFO");
+			karl.complain("WARNING");
+			karl.complain("ERROR");
+			break;
+		case Harl::WARNING:
+			karl.complain("WARNING");
+			karl.complain("ERROR");
+			break;
+		case Harl::ERROR:
+			karl.complain("ERROR");
+			break;
+		default:
+			return(error_badInput());
+	}
 	return (0);
 }
