d8e1ff0dd22702bfdcafbe61bac733860ec9030a
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-10 23:01:49 +0200

    Confirmed constructor is working with default params as expected by expanding the main.cpp tests

--- Module02/ex00/main.cpp
+++ Module02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/10 19:14:51 by limartin      #+#    #+#                 */
-/*   Updated: 2022/04/10 19:19:45 by limartin      ########   odam.nl         */
+/*   Updated: 2022/04/10 23:00:34 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 int		main(void)
 {
 	Fixed inst;
+	Fixed inst2(2);
 	std::cout << inst.getRawBits() << std::endl;
+	std::cout << inst2.getRawBits() << std::endl;
 	return (0);
 }
