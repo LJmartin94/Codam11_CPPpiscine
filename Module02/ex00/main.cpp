8fb355fd2407d6b932f0eeb6579a8ae5a147124c
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-10 22:45:02 +0200

    Made small main to test

---
+++ Module02/ex00/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: limartin <limartin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/04/10 19:14:51 by limartin      #+#    #+#                 */
+/*   Updated: 2022/04/10 19:19:45 by limartin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream> //stream operators
+
+int		main(void)
+{
+	Fixed inst;
+	std::cout << inst.getRawBits() << std::endl;
+	return (0);
+}
