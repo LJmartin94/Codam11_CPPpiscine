37df3e0a451435b7b1a2e40f904b03501d06f592
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-18 16:04:36 +0200

    Made the main a copy of the main specified in the subject for ex02

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/10 19:14:51 by limartin      #+#    #+#                 */
-/*   Updated: 2022/04/13 20:21:23 by limartin      ########   odam.nl         */
+/*   Updated: 2022/04/18 16:01:30 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,17 @@
 int		main( void )
 {
 	Fixed 		a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	a = Fixed( 1234.4321f );
-
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-
-	std::cout << "a is " << a.toInt() << " as an integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as an integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as an integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as an integer" << std::endl;
-
-	std::cout << "a is " << a.toFloat() << " as a float" << std::endl;
-	std::cout << "b is " << b.toFloat() << " as a float" << std::endl;
-	std::cout << "c is " << c.toFloat() << " as a float" << std::endl;
-	std::cout << "d is " << d.toFloat() << " as a float" << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 	
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b )<< std::endl;
+
 	return (0);
 }
