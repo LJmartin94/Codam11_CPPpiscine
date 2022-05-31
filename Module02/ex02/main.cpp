50737ab1a5fbc11e122fb57c0b804ea568d876ff
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-05-31 15:46:53 +0200

    Refactored the testing of the comparisson ops in the main to be a little cleaner.

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/10 19:14:51 by limartin      #+#    #+#                 */
-/*   Updated: 2022/05/03 17:21:49 by limartin      ########   odam.nl         */
+/*   Updated: 2022/05/31 15:45:14 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int		main( void )
 {
+	bool testComparisonOps = false;
 	Fixed 		a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
@@ -28,43 +29,52 @@ int		main( void )
 
 	std::cout << Fixed::max( a, b )<< std::endl;
 
-	Fixed		lilGuy( 1.0f );
-	Fixed		bigGuy( 2.0f );
-	Fixed		sameGuy(lilGuy);
+	if (testComparisonOps)
+	{
+		Fixed		lilGuy( 1.0f );
+		Fixed		bigGuy( 2.0f );
+		Fixed		sameGuy(lilGuy);
 
-	if (bigGuy > lilGuy)
-		std::cout << "good" << std::endl;
-	if (lilGuy < bigGuy)
-		std::cout << "good" << std::endl;
-	if (bigGuy >= lilGuy)
-		std::cout << "good" << std::endl;
-	if (sameGuy >= lilGuy)
-		std::cout << "good" << std::endl;
-	if (lilGuy <= bigGuy)
-		std::cout << "good" << std::endl;
-	if (sameGuy <= lilGuy)
-		std::cout << "good" << std::endl;
-	if (sameGuy == lilGuy)
-		std::cout << "good" << std::endl;
-	if (bigGuy != lilGuy)
-		std::cout << "good" << std::endl;
-	
-	if (!(bigGuy > lilGuy))
-		std::cout << "uh oh" << std::endl;
-	if (!(lilGuy < bigGuy))
-		std::cout << "uh oh" << std::endl;
-	if (!(bigGuy >= lilGuy))
-		std::cout << "uh oh" << std::endl;
-	if (!(sameGuy >= lilGuy))
-		std::cout << "uh oh" << std::endl;
-	if (!(lilGuy <= bigGuy))
-		std::cout << "uh oh" << std::endl;
-	if (!(sameGuy <= lilGuy))
-		std::cout << "uh oh" << std::endl;
-	if (!(sameGuy == lilGuy))
-		std::cout << "uh oh" << std::endl;
-	if (!(bigGuy != lilGuy))
-		std::cout << "uh oh" << std::endl;
+		if (bigGuy > lilGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+
+		if (lilGuy < bigGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+
+		if (bigGuy >= lilGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+
+		if (sameGuy >= lilGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+
+		if (lilGuy <= bigGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+
+		if (sameGuy <= lilGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+
+		if (sameGuy == lilGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+
+		if (bigGuy != lilGuy)
+			std::cout << "good" << std::endl;
+		else
+			std::cout << "uh oh" << std::endl;
+	}
 
 	return (0);
 }
