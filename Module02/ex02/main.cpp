2a087ef34624b57f25306edc9464ee799999d06c
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-06-07 11:55:50 +0200

    Wrote extra tests for experimenting with floats and multiplication, 'hid' them behind a boolean you can switch on or off in the main.

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/10 19:14:51 by limartin      #+#    #+#                 */
-/*   Updated: 2022/06/06 15:21:05 by limartin      ########   odam.nl         */
+/*   Updated: 2022/06/07 11:51:28 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 int		main( void )
 {
 	bool testComparisonOps = false;
+	bool testFloatsAndMultiplication = false;
 	Fixed 		a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
+	std::cout << "Tests specified by subject:" << std::endl;
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
@@ -28,54 +30,69 @@ int		main( void )
 	std::cout << Fixed::max( a, b )<< std::endl;
 	std::cout << std::endl;
 
-	Fixed const c(8385000);
-	std::cout << c << std::endl;
-	Fixed const d( Fixed( 8385.0f ) * Fixed( 1000.0f ));
-	std::cout << d << std::endl;
-	std::cout << std::endl;
+	if (testFloatsAndMultiplication)
+	{
+		Fixed const c(8388607.749f);
+		std::cout << "Largest number you can store using the float constructor: " << c << std::endl;
+		Fixed cpp;
+		cpp.setRawBits(INT_MAX);
+		std::cout << "Largest number you can possibly store:                    " << cpp << std::endl;
+		Fixed wut(8388608); //one bit larger than the largest number it can represent
+		--wut;
+		std::cout << "Storing 8388608 and underflowing the overflow:            " << wut << std::endl;
+	
+		std::cout << std::endl << "Testing some multiplications:" << std::endl;
+		Fixed const d( Fixed( 8388.608f ) * Fixed( 1000.0f ));
+		std::cout << d << std::endl;
+		Fixed const e( Fixed( 2896.3092f ) * Fixed( 2896.3092f ));
+		std::cout << e << std::endl;
+		std::cout << std::endl;
 
-	Fixed minimal;
-	++minimal;
-	Fixed temp (minimal * Fixed(1.0f));
-	std::cout << temp << std::endl;
-	temp = (Fixed(1.0f) * minimal);
-	std::cout << temp << std::endl;
-	std::cout << std::endl;
+		Fixed minimal;
+		++minimal;
+		Fixed temp (minimal * Fixed(1.0f));
+		std::cout << temp << std::endl;
+		temp = (Fixed(1.0f) * minimal);
+		std::cout << temp << std::endl;
+		temp = cpp * minimal;
+		std::cout << temp << std::endl;
+		std::cout << std::endl;
 
-	++minimal;
-	temp = (Fixed(0.5f) * minimal);
-	std::cout << temp << std::endl;
-	temp = (minimal * Fixed(0.5f));
-	std::cout << temp << std::endl;
-	std::cout << std::endl;
+		++minimal;
+		temp = (Fixed(0.5f) * minimal);
+		std::cout << temp << std::endl;
+		temp = (minimal * Fixed(0.5f));
+		std::cout << temp << std::endl;
+		std::cout << std::endl;
 
-	++minimal;
-	temp = (Fixed(0.34f) * minimal);
-	std::cout << temp << std::endl;
-	temp = (minimal * Fixed(0.34f));
-	std::cout << temp << std::endl;
-	std::cout << std::endl;
+		++minimal;
+		temp = (Fixed(0.34f) * minimal);
+		std::cout << temp << std::endl;
+		temp = (minimal * Fixed(0.34f));
+		std::cout << temp << std::endl;
+		std::cout << std::endl;
 
-	++minimal;
-	temp = (Fixed(0.25f) * minimal);
-	std::cout << temp << std::endl;
-	temp = (minimal * Fixed(0.25f));
-	std::cout << temp << std::endl;
-	std::cout << std::endl;
+		++minimal;
+		temp = (Fixed(0.25f) * minimal);
+		std::cout << temp << std::endl;
+		temp = (minimal * Fixed(0.25f));
+		std::cout << temp << std::endl;
+		std::cout << std::endl;
 
-	++minimal;
-	temp = (Fixed(0.21f) * minimal);
-	std::cout << temp << std::endl;
-	temp = (minimal * Fixed(0.21f));
-	std::cout << temp << std::endl;
-	std::cout << std::endl;
+		++minimal;
+		temp = (Fixed(0.21f) * minimal);
+		std::cout << temp << std::endl;
+		temp = (minimal * Fixed(0.21f));
+		std::cout << temp << std::endl;
+		std::cout << std::endl;
 
-	++minimal;
-	temp = (Fixed(0.17f) * minimal);
-	std::cout << temp << std::endl;
-	temp = (minimal * Fixed(0.17f));
-	std::cout << temp << std::endl;
-	std::cout << std::endl;
+		++minimal;
+		temp = (Fixed(0.17f) * minimal);
+		std::cout << temp << std::endl;
+		temp = (minimal * Fixed(0.17f));
+		std::cout << temp << std::endl;
+		std::cout << std::endl;
+	}
 
 	if (testComparisonOps)
 	{
