e02c131f0ce0be2b11afba8eb1bcbc4b3ea2fcdc
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-07-20 18:15:31 +0200

    Changed spaces to tabs.

--- Module02/ex03/Point.hpp
+++ Module02/ex03/Point.hpp
@@ -5,8 +5,8 @@
 /*                                                     +:+                    */
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/07/16 12:03:35 by limartin      #+#    #+#                 */
-/*   Updated: 2022/07/18 17:48:57 by limartin      ########   odam.nl         */
+/*   Created: 2022/07/20 18:12:11 by limartin      #+#    #+#                 */
+/*   Updated: 2022/07/20 18:12:58 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,29 @@
 
 class Point
 {
-    public:
+	public:
 
-    Point();                                        // Default constructor.
-    Point( Fixed x, Fixed y);                       // Parameterised constructor.
-    Point( const Point& copy);                      // Copy constructor.
-    Point& operator= ( const Point& assignment );	// Assignment Operator Overload.
-    ~Point();                                       // Default destructor.
+	Point();										// Default constructor.
+	Point( Fixed x, Fixed y );					  	// Parameterised constructor.
+	Point( const Point& copy );					 	// Copy constructor.
+	Point& operator= ( const Point& assignment );	// Assignment Operator Overload.
+	~Point();										// Default destructor.
 
-    Fixed&  getPointX() const; // X getters and setters.
-    void    setPointX( const Fixed& copy );
-    void    setPointX( int n );
-    void    setPointX( float n );
+	const Fixed& 	getPointX() const; // X getters and setters.
+	void			setPointX( const Fixed& copy );
+	void			setPointX( int n );
+	void			setPointX( float n );
 
-    Fixed&  getPointY() const; // Y getters and setters.
-    void    setPointY( const Fixed& copy );
-    void    setPointY( int n );
-    void    setPointY( float n );
+	const Fixed&	getPointY() const; // Y getters and setters.
+	void			setPointY( const Fixed& copy );
+	void			setPointY( int n );
+	void			setPointY( float n );
 
-    private:
-    Fixed const _x;
-    Fixed const _y;
+	private:
+	Fixed const _x;
+	Fixed const _y;
 
-    // https://print-graph-paper.com/virtual-graph-paper
+	// https://print-graph-paper.com/virtual-graph-paper
 
 };
 
