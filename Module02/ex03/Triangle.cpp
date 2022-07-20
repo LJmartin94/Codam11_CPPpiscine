ebac30028d6748463935ddf2d387df0618ffbe0b
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-07-20 18:19:57 +0200

    Decided the bsp method should be implemented in a Triangle class of some sort for now.

--- Module02/ex03/Triangle.cpp
+++ Module02/ex03/Triangle.cpp
@@ -1,20 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.cpp                                           :+:    :+:            */
+/*   Triangle.cpp                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/04/10 19:14:51 by limartin      #+#    #+#                 */
-/*   Updated: 2022/07/20 17:47:17 by limartin      ########   odam.nl         */
+/*   Created: 2022/07/20 17:34:30 by limartin      #+#    #+#                 */
+/*   Updated: 2022/07/20 17:35:42 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Triangle.hpp"
-// #include <iostream> //stream operators
-
-int		main( void )
-{
-
-	return (0);
-}
--- Module02/ex03/Triangle.hpp
+++ Module02/ex03/Triangle.hpp
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.cpp                                           :+:    :+:            */
+/*   Triangle.hpp                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/04/10 19:14:51 by limartin      #+#    #+#                 */
-/*   Updated: 2022/07/20 17:47:17 by limartin      ########   odam.nl         */
+/*   Created: 2022/07/20 17:36:41 by limartin      #+#    #+#                 */
+/*   Updated: 2022/07/20 17:46:07 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Triangle.hpp"
-// #include <iostream> //stream operators
+#ifndef TRIANGLE_HPP
+# define TRIANGLE_HPP
 
-int		main( void )
-{
+#include "Point.hpp"
 
-	return (0);
-}
+bool bsp( Point const a, Point const b, Point const c, Point const point );
+
+#endif
