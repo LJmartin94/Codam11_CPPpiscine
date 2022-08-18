dc19dd80176048fcec3c5a339b1e4239883b6bdd
limartin <lindsayjamesmartin94@gmail.com>
2022-08-18 14:15:05 +0200

    Made a directory with Templates for classes for all the following modules, perhaps more templates will be added in future.

---
+++ Templates/TemplateClass.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   TemplateClass.cpp                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 13:30:35 by lindsay       #+#    #+#                 */
+/*   Updated: 2022/08/18 14:10:30 by lindsay       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "TemplateClass.hpp"
+
+// Constructors & Destructors
+////////////////////////////////////////////////////////////////////////////////
+
+// Default constructor
+TemplateClass::TemplateClass()
+{
+	if (TemplateClass_DEBUG_MESSAGES)
+		std::cout << "Default constructor called." << std::endl;
+	return;
+}
+
+// Copy constructor
+TemplateClass::TemplateClass( const TemplateClass& copy)
+{
+	if (TemplateClass_DEBUG_MESSAGES)
+		std::cout << "Copy constructor called." << std::endl;
+	*this = copy;
+	return;
+}
+
+// Destructor
+TemplateClass::~TemplateClass()
+{
+	if (TemplateClass_DEBUG_MESSAGES)
+		std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Operator overloads
+////////////////////////////////////////////////////////////////////////////////
+
+// Assignment operator overload (deep copy)
+TemplateClass& TemplateClass::operator= ( const TemplateClass& assignment )
+{
+	if (TemplateClass_DEBUG_MESSAGES && TemplateClass_ADD_VERBOSE)
+		std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &assignment)
+	{
+		//TODO: REQUIRES PER CLASS IMPLEMENTATION
+	}
+	return(*this);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// External to class
+////////////////////////////////////////////////////////////////////////////////
+
+//Stream operator overload
+std::ostream& operator<< ( std::ostream& o, const TemplateClass& i )
+{
+	//TODO: REQUIRES PER CLASS IMPLEMENTATION
+	o << i;
+	return (o);
+}
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ Templates/TemplateClass.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   TemplateClass.hpp                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 13:30:42 by lindsay       #+#    #+#                 */
+/*   Updated: 2022/08/18 14:12:03 by lindsay       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//TODO: Make sure this is all-caps!
+#ifndef TEMPLATECLASS_HPP
+# define TEMPLATECLASS_HPP
+
+#include <iostream> // Stream operators
+
+# define TemplateClass_DEBUG_MESSAGES 1
+# define TemplateClass_ADD_VERBOSE 1
+
+class TemplateClass
+{
+	// Constructors & Destructors
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	TemplateClass();								// Default constructor
+	TemplateClass(const TemplateClass& copy);		// Copy constructor
+	~TemplateClass();								// Destructor
+	////////////////////////////////////////////////////////////////////////////
+
+	// Operator overloads
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	TemplateClass& operator= (const TemplateClass& assignment);	// Assignment operator
+	////////////////////////////////////////////////////////////////////////////
+};
+
+// Stream operator overload
+std::ostream& operator<< ( std::ostream& o, TemplateClass const & i );
+
+#endif
