889cbc669604c59be30b516046bb1c05caccccfc
limartin <lindsayjamesmartin94@gmail.com>
2022-08-18 14:47:28 +0200

    Added the private attributes to the claptrap class hpp, and set their default values in their default constructor.

--- Module03/ex00/ClapTrap.hpp
+++ Module03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/18 13:30:42 by lindsay       #+#    #+#                 */
-/*   Updated: 2022/08/18 14:31:53 by lindsay       ########   odam.nl         */
+/*   Updated: 2022/08/18 14:41:27 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ class ClapTrap
 	// Constructors & Destructors
 	////////////////////////////////////////////////////////////////////////////
 	public:
-	ClapTrap();								// Default constructor
+	// Default constructor
+	ClapTrap(std::string name="No One", int hp=10, int nrg=10, int dmg=0);
+	
 	ClapTrap(const ClapTrap& copy);			// Copy constructor
 	~ClapTrap();							// Destructor
 	////////////////////////////////////////////////////////////////////////////
@@ -34,10 +36,25 @@ class ClapTrap
 	ClapTrap& operator= (const ClapTrap& assignment);	// Assignment operator
 	////////////////////////////////////////////////////////////////////////////
 
+	// Pubic member variables & methods
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	////////////////////////////////////////////////////////////////////////////
+
 	// Private member variables & methods
 	////////////////////////////////////////////////////////////////////////////
 	private:
+	std::string _name;
+	int			_hp;
+	int			_energy;
+	int			_dmg;
 	////////////////////////////////////////////////////////////////////////////
+
+	// Accessors
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	////////////////////////////////////////////////////////////////////////////
+
 };
 
 // Stream operator overload
