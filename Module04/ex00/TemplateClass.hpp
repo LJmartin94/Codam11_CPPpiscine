7c37282a0de6668ab75091a7663c1d54069d5047
limartin <lindsayjamesmartin94@gmail.com>
2022-08-21 11:22:22 +0200

    Added template files, updated Makefile already.

---
+++ Module04/ex00/TemplateClass.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   TemplateClass.hpp                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 13:30:42 by lindsay       #+#    #+#                 */
+/*   Updated: 2022/08/18 20:12:51 by lindsay       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//TODO: Make sure this is all-caps!
+#ifndef TEMPLATECLASS_HPP
+# define TEMPLATECLASS_HPP
+
+#include <iostream> // Stream operators
+
+# define TemplateClass_DEBUG_MESSAGES 1
+# define TemplateClass_ADD_VERBOSE 1
+
+class TemplateClass
+{
+	// Constructors & Destructors
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	TemplateClass();								// Default constructor
+	TemplateClass(const TemplateClass& copy);		// Copy constructor
+	~TemplateClass();								// Destructor
+	////////////////////////////////////////////////////////////////////////////
+
+	// Operator overloads
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	TemplateClass& operator= (const TemplateClass& assignment);	// Assignment operator
+	////////////////////////////////////////////////////////////////////////////
+
+	// Pubic member variables & methods
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	////////////////////////////////////////////////////////////////////////////
+
+	// Private member variables & methods
+	////////////////////////////////////////////////////////////////////////////
+	private:
+	////////////////////////////////////////////////////////////////////////////
+
+	// Accessors
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	////////////////////////////////////////////////////////////////////////////
+};
+
+// Stream operator overload
+std::ostream& operator<< (std::ostream& o, TemplateClass const & i);
+
+#endif
