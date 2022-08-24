6d4dde152efb3f455ed36d5dfb2c1861dc215cb2
limartin <lindsayjamesmartin94@gmail.com>
2022-08-24 14:02:33 +0200

    Wrote the main

--- Module04/ex00/main.cpp
+++ Module04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/18 13:30:46 by lindsay       #+#    #+#                 */
-/*   Updated: 2022/08/18 14:24:27 by lindsay       ########   odam.nl         */
+/*   Updated: 2022/08/24 14:01:56 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 int main(void)
 {
+	const Animal* meta = new Animal();
+	const Animal* i = new Cat();
+	const Animal* j = new Dog();
+	const WrongAnimal* w = new WrongCat();
+
+	std::cout << meta->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << w->getType() << " " << std::endl;
+	
+	meta->makeSound();
+	i->makeSound();
+	j->makeSound();
+	w->makeSound();
+
+	delete meta;
+	delete i;
+	delete j;
+	delete w;
+	
 	return(0);
 }
