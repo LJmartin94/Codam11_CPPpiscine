9a5e26439d4b7f66dcc4f52f12b75ee539a1b212
limartin <lindsayjamesmartin94@gmail.com>
2022-09-05 18:19:03 +0200

    Wrote tests to prove everything is working, updated Makefile to remove the appropriate files afterwards.

--- Module05/ex03/main.cpp
+++ Module05/ex03/main.cpp
@@ -6,13 +6,14 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/18 13:30:46 by lindsay       #+#    #+#                 */
-/*   Updated: 2022/09/05 16:40:13 by lindsay       ########   odam.nl         */
+/*   Updated: 2022/09/05 18:13:28 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-#include "Form.hpp"
+#include "Intern.hpp"
 
+#include "Form.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
@@ -20,51 +21,66 @@
 int main(void)
 {
 	Bureaucrat bob("Bob", 1);
-	Bureaucrat igor("Igor", 150);
+	Intern underbob;
+	std::cout << underbob << std::endl;
+	// VALID SYNTAX (to me)//
+	Form *a = underbob.makeForm("Robotomy request form", "Bender");
+	delete a;
+	a = underbob.makeForm("robotomy request form", "Bender");
+	delete a;
+	a = underbob.makeForm("RobotomyRequestForm", "Bender");
+	delete a;
+	a = underbob.makeForm("RobOtomyREQUEst", "Bender");
+	delete a;
+	a = underbob.makeForm("robotomyrequest", "Bender");
+	delete a;
+	a = underbob.makeForm("Robotomy request form form form form", "Bender");
+	delete a;
+	a = underbob.makeForm("     R o botomy   re quest   fo rm      ", "Bender");
+	delete a;
+	a = underbob.makeForm("Robotomy request form, please", "Bender");
+	bob.signForm(*a);
+	bob.executeForm(*a);
+	delete a;
 
-	ShrubberyCreationForm shrub("Shrobert");
-	std::cout << shrub << std::endl;
-	RobotomyRequestForm marvin("Marvin");
-	std::cout << marvin << std::endl;
-	PresidentialPardonForm nicola("Nicola Sturgeon");
-	std::cout << nicola << std::endl;
 
+	a = underbob.makeForm("Presidential pardon form", "Bender");
+	bob.signForm(*a);
+	bob.executeForm(*a);
+	delete a;
+
+	a = underbob.makeForm("Shrubbery Creation form", "Bender");
+	bob.signForm(*a);
+	bob.executeForm(*a);
+	delete a;
+
+	// INVALID SYNTAX (to me)//
 	try
 	{
-		nicola.execute(bob);
+		Form *b = underbob.makeForm("Robotomy Robotomy request form", "Bender");
+		delete b;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-
-	bob.executeForm(nicola);
-	igor.executeForm(nicola);
-	bob.signForm(nicola);
-	igor.executeForm(nicola);
-	bob.executeForm(nicola);
-
-	ShrubberyCreationForm ni("The knights who say Ni");
-	bob.signForm(shrub);
-	shrub = ni;
-	std::cout << shrub;
-
-	bob.signForm(shrub);
-	bob.executeForm(shrub);
-
-	std::cout << nicola;
-	bob.signForm(nicola);
-	bob.executeForm(nicola);
-
-	std::cout << marvin;
-	bob.signForm(marvin);
-	bob.executeForm(marvin);
-	bob.executeForm(marvin);
-	bob.executeForm(marvin);
-	bob.executeForm(marvin);
-	bob.executeForm(marvin);
-	bob.executeForm(marvin);
-
-		
+	try
+	{
+		Form *b = underbob.makeForm("A Robotomy request form, please", "Bender");
+		delete b;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Form *b = underbob.makeForm("Robotomy request, please", "Bender");
+		delete b;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return(0);
 }
