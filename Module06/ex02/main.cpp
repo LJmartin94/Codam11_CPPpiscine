87c344769083b99473bec2ff7490f74761139ebe
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-09-21 23:54:58 +0200

    Wrote one of the identify functions.

--- Module06/ex02/main.cpp
+++ Module06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/18 13:30:46 by lindsay       #+#    #+#                 */
-/*   Updated: 2022/09/21 23:46:15 by limartin      ########   odam.nl         */
+/*   Updated: 2022/09/21 23:53:35 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,49 +39,80 @@ Base *generate(void)
 	}
 }
 
+void identify(Base* p)
+{
+	if (dynamic_cast<ClassA*>(p) != NULL)
+		std::cout << "A";
+	else if (dynamic_cast<ClassB*>(p) != NULL)
+		std::cout << "B";
+	else if (dynamic_cast<ClassC*>(p) != NULL)
+		std::cout << "C";
+	return;
+}
+
+void identify(Base& p)
+{
+	(void)p;
+}
+
+
 int main(void)
 {
 	srand(time(NULL));
 	
 	std::cout << std::endl;
 	Base *mystery = generate();
-	std::cout << "Identify by pointer: " << std::endl;
+	std::cout << "Identify by pointer: ";
+	identify(mystery);
+	std::cout << std::endl;
 	std::cout << "Identify by reference: " << std::endl;
 	delete mystery;
 
 	std::cout << std::endl;
 	mystery = generate();
-	std::cout << "Identify by pointer: " << std::endl;
+	std::cout << "Identify by pointer: ";
+	identify(mystery);
+	std::cout << std::endl;
 	std::cout << "Identify by reference: " << std::endl;
 	delete mystery;
 
 	std::cout << std::endl;
 	mystery = generate();
-	std::cout << "Identify by pointer: " << std::endl;
+	std::cout << "Identify by pointer: ";
+	identify(mystery);
+	std::cout << std::endl;
 	std::cout << "Identify by reference: " << std::endl;
 	delete mystery;
 
 	std::cout << std::endl;
 	mystery = generate();
-	std::cout << "Identify by pointer: " << std::endl;
+	std::cout << "Identify by pointer: ";
+	identify(mystery);
+	std::cout << std::endl;
 	std::cout << "Identify by reference: " << std::endl;
 	delete mystery;
 
 	std::cout << std::endl;
 	mystery = generate();
-	std::cout << "Identify by pointer: " << std::endl;
+	std::cout << "Identify by pointer: ";
+	identify(mystery);
+	std::cout << std::endl;
 	std::cout << "Identify by reference: " << std::endl;
 	delete mystery;
 
 	std::cout << std::endl;
 	mystery = generate();
-	std::cout << "Identify by pointer: " << std::endl;
+	std::cout << "Identify by pointer: ";
+	identify(mystery);
+	std::cout << std::endl;
 	std::cout << "Identify by reference: " << std::endl;
 	delete mystery;
 
 	std::cout << std::endl;
 	mystery = generate();
-	std::cout << "Identify by pointer: " << std::endl;
+	std::cout << "Identify by pointer: ";
+	identify(mystery);
+	std::cout << std::endl;
 	std::cout << "Identify by reference: " << std::endl;
 	delete mystery;
 	
