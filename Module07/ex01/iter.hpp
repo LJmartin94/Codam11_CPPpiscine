d9577f347b98f7c21f08198c8b539c8182ec6e71
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-09-25 15:00:55 +0200

    Aded files from ex00 as basis for ex01

---
+++ Module07/ex01/iter.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   iter.hpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 13:30:42 by lindsay       #+#    #+#                 */
+/*   Updated: 2022/09/25 15:00:00 by limartin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream> // Stream operators
+
+// template<typename T>
+// void    swap(T& x, T& y)
+// {
+//     T temp;
+//     temp = x;
+//     x = y;
+//     y = temp;
+//     return;
+// }
+
+// template<typename T>
+// T   min(const T& x, const T& y)
+// {
+//     if(x < y)
+//         return x;
+//     return y;
+// }
+
+// template<typename T>
+// T   max(const T& x, const T& y)
+// {
+//     if(x > y)
+//         return x;
+//     return y;
+// }
+
+#endif
---
+++ Module07/ex01/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 13:30:46 by lindsay       #+#    #+#                 */
+/*   Updated: 2022/09/25 14:58:48 by limartin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+int main(void)
+{
+	// int a = 2;
+	// int b = 3;
+
+	// ::swap( a, b );
+	// std::cout << "a = " << a << ", b = " << b << std::endl;
+	// std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	// std::string c = "chaine1";
+	// std::string d = "chaine2";
+
+	// ::swap( c, d );
+	// std::cout << "c = " << c << ", d = " << d << std::endl;
+	// std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	return(0);
+}
