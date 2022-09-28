d5f5f63c188b157572d2359dd15c37237898752f
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-09-28 04:15:32 +0200

    We've reached the final stretch. Adding the mock-up files for CPP08 ex02.

---
+++ Module08/ex02/TemplateClass.hpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   TemplateClass.hpp                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 13:30:42 by lindsay       #+#    #+#                 */
+/*   Updated: 2022/09/14 15:39:53 by lindsay       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//TODO: Make sure this is all-caps!
+#ifndef TEMPLATECLASS_HPP
+# define TEMPLATECLASS_HPP
+
+#include <iostream> // Stream operators
+
+# ifndef TemplateClass_DEBUG_MESSAGES 
+#  define TemplateClass_DEBUG_MESSAGES 1
+# endif
+
+# ifndef TemplateClass_ADD_VERBOSE 
+#  define TemplateClass_ADD_VERBOSE 1
+# endif
+
+
+class TemplateClass
+{
+	// Constructors & Destructors
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	TemplateClass();								// Default constructor
+	TemplateClass(const TemplateClass& copy);		// Copy constructor
+	~TemplateClass();								// Destructor
+	////////////////////////////////////////////////////////////////////////////
+
+	// Operator overloads
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	TemplateClass& operator= (const TemplateClass& assignment);	// Assignment operator
+	////////////////////////////////////////////////////////////////////////////
+
+	// Pubic member variables & methods
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	////////////////////////////////////////////////////////////////////////////
+
+	// Protected member variables & methods
+	////////////////////////////////////////////////////////////////////////////
+	protected:
+	////////////////////////////////////////////////////////////////////////////
+
+	// Private member variables & methods
+	////////////////////////////////////////////////////////////////////////////
+	private:
+	////////////////////////////////////////////////////////////////////////////
+
+	// Accessors
+	////////////////////////////////////////////////////////////////////////////
+	public:
+	////////////////////////////////////////////////////////////////////////////
+
+	// Nested classes
+	////////////////////////////////////////////////////////////////////////////
+	private:
+	////////////////////////////////////////////////////////////////////////////
+};
+
+// Stream insertion operator overload
+std::ostream& operator<< (std::ostream& o, TemplateClass const & i);
+
+#endif
